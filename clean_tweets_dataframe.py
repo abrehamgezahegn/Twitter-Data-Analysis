d2128d64cf9e1bfa9911bcfc7168d65c43da9d96
abrehamgezahegn <gezahegnabereham@gmail.com>
2022-04-26 21:14:32 +0300

    fix: convert to datetime, sensitivity && location bug fixxes

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -26,15 +26,15 @@ class Clean_Tweets:
         
     
         return df
-    def convert_to_datetime(self, df:pd.DataFrame)->pd.DataFrame:
-        """
-        convert column to datetime
-        """
-        df['created_at'] = pd.to_datetime(df['created_at'], format='%Y%m%d')
-        
-        df = df[df['created_at'] >= '2020-12-31']
-        
-        return df
+    def convert_to_datetime(self, df: pd.DataFrame) -> pd.DataFrame:
+        """convert column to datetime."""
+        self.df['created_at'] = pd.to_datetime(
+            df['created_at'], errors='coerce')
+
+        self.df = df[df['created_at'] >= '2020-12-31']
+
+        # self.convert_to_numbers(self.df)
+        return self.df
     
     def convert_to_numbers(self, df:pd.DataFrame)->pd.DataFrame:
         """Convert columns like polarity, subjectivity, favorite_count, retweet_count to numbers."""
