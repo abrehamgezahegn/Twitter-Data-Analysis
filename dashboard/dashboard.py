82485f23bb34b16ba461451b4c79be4b51baff93
abrehamgezahegn <gezahegnabereham@gmail.com>
2022-04-29 19:57:44 +0300

    feat: added location and languge filter in dashboard

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -20,6 +20,26 @@ def selectHashTag():
         df = df[np.isin(df, hashTags).any(axis=1)]
         st.write(df)
 
+def selectLocAndAuth():
+    df = loadData()
+    location = st.multiselect("choose Location of tweets", list(df['place_coordinate'].unique()))
+    lang = st.multiselect("choose Language of tweets", list(df['language'].unique()))
+
+    if location and not lang:
+        df = df[np.isin(df, location).any(axis=1)]
+        st.write(df)
+    elif lang and not location:
+        df = df[np.isin(df, lang).any(axis=1)]
+        st.write(df)
+    elif lang and location:
+        location.extend(lang)
+        df = df[np.isin(df, location).any(axis=1)]
+        st.write(df)
+    else:
+        st.write(df)
 
 st.title("Data Display")
 selectHashTag()
+
+st.markdown("<p style='padding:10px; background-color:#000000;color:#00ECB9;font-size:16px;border-radius:10px;'>Section Break</p>", unsafe_allow_html=True)
+selectLocAndAuth()
