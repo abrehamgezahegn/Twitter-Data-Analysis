caad7a1bdc79995fb438af450defa947b0fe500b
abrehamgezahegn <gezahegnabereham@gmail.com>
2022-04-29 19:55:19 +0300

    feat: added hashtag display to dashboard

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,6 +1,25 @@
-import streamlit as st
+import numpy as np
 import pandas as pd
+import streamlit as st
+import altair as alt
+from wordcloud import WordCloud
+import plotly.express as px
+from add_data import db_execute_fetch
+
+st.set_page_config(page_title="Day 5", layout="wide")
+
+def loadData():
+    query = "select * from TweetInformation"
+    df = db_execute_fetch(query, dbName="tweets", rdf=True)
+    return df
+
+def selectHashTag():
+    df = loadData()
+    hashTags = st.multiselect("choose combination of hashtags", list(df['hashtags'].unique()))
+    if hashTags:
+        df = df[np.isin(df, hashTags).any(axis=1)]
+        st.write(df)
 
 
-st.title('Twitter topic modeling and sentiment analysis')
-
+st.title("Data Display")
+selectHashTag()
